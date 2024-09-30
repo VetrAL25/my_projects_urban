a83ad696bb67aae96ce858776361aff3d2662210
VetrAL25 <vetranapa@gmail.com>
2024-09-30 17:37:39 +0300

    ball.py

--- ball.py
+++ ball.py
@@ -1,12 +1,8 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 students = sorted(students) # перевел в список
-print (type(grades))
-print (type(students))
-print(students) # студенты по алфавиту
-# av_ball = {}
-print (sum(grades[0])/len(grades[0])) #ну это средняя оценка
-# а как на всех вывести? не так же:
-print (sum(grades[0])/len(grades[0]), sum(grades[1])/len(grades[1]), sum(grades[2])/len(grades[2]), sum(grades[3])/len(grades[3]), sum(grades[4])/len(grades[4]))
-# а если их будет 20 или 120???
-# и дальше - ка сопоставить соответствующие элементы двух списков... Туплю(((
+# print (type(grades))
+# print (type(students))
+# print(students) # студенты по алфавиту
+# sum(grades[0])/len(grades[0])  - ну это средняя оценка
+print (students[0], sum(grades[0])/len(grades[0]), students[1], sum(grades[1])/len(grades[1]), students[2], sum(grades[2])/len(grades[2]), students[3], sum(grades[3])/len(grades[3]), students[4], sum(grades[4])/len(grades[4]))
