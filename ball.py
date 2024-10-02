d797cd48f40914bc9584ff1fb49443f67617f4dd
VetrAL25 <vetranapa@gmail.com>
2024-10-02 15:49:43 +0300

    ball.py

--- ball.py
+++ ball.py
@@ -1,8 +1,6 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
-students = sorted(students) # перевел в список
-# print (type(grades))
-# print (type(students))
-# print(students) # студенты по алфавиту
-# sum(grades[0])/len(grades[0])  - ну это средняя оценка
-print (students[0], sum(grades[0])/len(grades[0]), students[1], sum(grades[1])/len(grades[1]), students[2], sum(grades[2])/len(grades[2]), students[3], sum(grades[3])/len(grades[3]), students[4], sum(grades[4])/len(grades[4]))
+av_grades = [(sum (x)/ len (x)) for x in grades]
+sstudents = sorted(students)
+out = (dict(zip (sstudents, av_grades)))  
+print (out)
