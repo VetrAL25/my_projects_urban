43663d483a3b97da1dd85d1b687f3f57bee33cbc
VetrAL25 <vetranapa@gmail.com>
2024-10-13 00:45:26 +0300

    Add files via upload

--- module_3_4.py
+++ module_3_4.py
@@ -1,11 +1,8 @@
 def single_root_words (root_word, *other_word):
     same_word = []
     for i in other_word:
-        if i.lower() in root_word.lower():
+        if i.lower() in root_word.lower() or root_word.upper() in i.upper():
             same_word.append(i)
-    for j in other_word:
-        if root_word.upper() in j.upper():
-            same_word.append(j)
     return same_word
 res1 = single_root_words('ПИК', 'Пикап', 'кипа', 'ПиЛкА', 'тУПИк' ,'тРОПики')
 res2 = single_root_words('наборщик', 'боРЩ', 'роба', 'БОР', 'забор' ,'набор')
